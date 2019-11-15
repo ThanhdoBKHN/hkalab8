d3a41e6efa9d1862fd17e9c46a49182e4b8337b4
ThanhdoBKHN <41502627+ThanhdoBKHN@users.noreply.github.com>
2019-11-15 18:46:15 +0700

    Add files via upload

---
+++ src/java/controller/ControllerServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+
+import bll.ProductBLL;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ControllerServlet",
+        loadOnStartup = 1,
+        urlPatterns = {"/ControllerServlet"})
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+        super.init(servletConfig);
+
+        ProductBLL productBLL = new ProductBLL();
+        try {
+            getServletContext().setAttribute("newProducts", productBLL.getNewProducts(5));
+        } catch (NamingException ex) {
+            Logger.getLogger(ControllerServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+}
+
+@Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+    }
+}
